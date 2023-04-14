3eb813ef67a9ad179f3bb28e1d82f8b7a4e14422
Hilary Akpu <cc836170@student.reading.ac.uk>
2023-04-14 15:00:33 +0100

    Modify simple.py
    Modified script to provide better comments and use threading/multiprocessing throughout the Map Reduce stage i.e. in mapping, shuffling, and reducing

--- src/simple.py
+++ src/simple.py
@@ -24,6 +24,7 @@ def reduce_func(x, y):
 
 
 def pret_print(str_):
+    """display string while processing"""
     while True:
         print(str_)
         time.sleep(0.4)
@@ -41,43 +42,46 @@ if __name__ == '__main__':
 
     # Get the parent directory of main.py
     parent_directory = os.path.dirname(os.path.abspath(__file__))
-
     # Construct the path to config.yaml
     config_path = os.path.join(parent_directory, '..', 'config.yaml')
-
+    # get path to data from config file and read data
     with open(config_path, 'r') as file:
         yml = yaml.safe_load(file)
     path = os.path.join(parent_directory, '..', yml['path'])
     df = pd.read_csv(path, header=None)
     col1 = df.columns[0]
     map_in = df[col1].tolist()
-    # map_out = []
 
-
+    # Map Reduce using Threads and Multiprocessing
     for i in range(2):
-        if i == 0:
-            USE_MULTIPROCESSING = True  # Set to True to use multiprocessing, False to use threading
-        else:
-            USE_MULTIPROCESSING = False  # Set to True to use multiprocessing, False to use threading
-
         t1 = time.time()
-        if USE_MULTIPROCESSING:
+        if i == 0:
             pretty_print('multiprocessing')
+            # call function to continually print in background until Map reduce process is done
             threading.Thread(target=pret_print, daemon=True, args=('multiprocessing...', )).start()
+            # Map Reduce using mutliprocessing
             with concurrent.futures.ProcessPoolExecutor() as executor:
                 map_out = list(executor.map(map_func, map_in))
+                reduce_in = executor.submit(shuffle, map_out)
+                reduce_in = reduce_in.result()
+                for key, values in reduce_in.items():
+                    _ = executor.submit(reduce, reduce_func, values)
         else:
             pretty_print('threading')
+            # call function to continually print in background until Map reduce process is done
             threading.Thread(target=pret_print, daemon=True, args=('threading...', )).start()
+            # Map Reduce using Threads
             with concurrent.futures.ThreadPoolExecutor() as executor:
-                # executor.map(pret_print, [1, 2])
                 map_out = list(executor.map(map_func, map_in))
+                reduce_in = executor.submit(shuffle, map_out)
+                reduce_in = reduce_in.result()
+                for key, values in reduce_in.items():
+                    _ = executor.submit(reduce, reduce_func, values)
         t2 = time.time()
         print("Done\nElapsed time: {:.3} seconds\n".format(t2 - t1))
-    reduce_in = shuffle(map_out)
-    reduce_out = {}
 
-    for key, values in reduce_in.items():
-        reduce_out[key] = reduce(reduce_func, values)
+
+
+
 
 
